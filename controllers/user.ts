7b14c469532ce8961607d99028fd08a6aa6bd966
Shalini <shalini98279@gmail.com>
2023-03-05 12:11:49 +0530

    Custom Repo and functions using manager and repo

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { User } from '../entity/User'
 import { AppDataSource } from '../app'
+import {Not, LessThanOrEqual, MoreThanOrEqual, Like, In, IsNull} from 'typeorm'
 
 const homeDetail = async (req: Request, res: Response) => {
 
@@ -29,11 +30,11 @@ const homeDetail = async (req: Request, res: Response) => {
     // user.phone = '12345678'
     // let data = await AppDataSource.manager.save(user)
 
-    // let user = new User();
-    // user.name = 'dummy'
-    // user.email = 'dummy@gmail.com'
-    // user.phone = '23455'
-    // let data = await userRepository.save(user)
+    let user = new User();
+    user.name = 'userX'
+    user.email = 'userX@gmail.com'
+    user.phone = '234'
+    let data = await userRepository.save(user)
 
 
     //-------Update---------------------------------------------------------------------------
@@ -68,13 +69,43 @@ const homeDetail = async (req: Request, res: Response) => {
     //----truncate------------------------------------------------------------------------------
     // let data = await manager.clear(User)
 
-    let data = await userRepository.clear()
+    // let data = await userRepository.clear()
 
+    // -----------------------------select--------------------------------
+    // let data = await userRepository.find({
+    //     select: ["name", "email"]
+    // })
+
+    //------------------------------where----------------------------------
+    // let data = await userRepository.find({
+    //     where: [
+    //         { email:  }
+    //     ],
+    // })
+
+    //-----------------------------order--------------------------
+    // let data = await userRepository.find({
+    //     order:{
+    //         id: "DESC",
+    //         name: "ASC",
+    //     },
+    //     skip: 1,
+    //     take: 3
+    // })
+
+    //-----------------------------order--------------------------
+    // let data = await userRepository.findBy({
+    //     name:Not("demo")
+    //     name: Like(''%ra%')
+    //     id: Between(1, 4)
+     //    name: In(["demo"])
+     //    phone: IsNull()
+     // })
 
 
     res.json({
         test: 'ok',
-        data: data
+        data,
     })
 }
 
---
+++ repo/UserRepository.ts
@@ -0,0 +1,2 @@
+import { User } from '../entity/User'
+import { AppDataSource } from '../app'
